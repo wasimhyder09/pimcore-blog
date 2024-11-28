52ac6ba19d4059df326fe85cd5e8cd4cbe1f400e
Wasim Hyder <wasimhyder09@gmail.com>
2024-11-28 12:23:59 +0500

    Fixed syntax error

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -6,6 +6,7 @@ use Pimcore\Controller\FrontendController;
 use Pimcore\Model\DataObject\BlogPost;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends FrontendController {
   /**
